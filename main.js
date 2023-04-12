1bf5469c226934523da8cf32fb68bfe2dc25bcd3
alexcapricorngmailcom <107393171+alexcapricorngmailcom@users.noreply.github.com>
2023-04-12 17:08:29 +0300

    add

--- main.js
+++ main.js
@@ -42,3 +42,64 @@ function countMouses (arr1, arr2) {
 const counted = countMouses(mouses, deepMouses);
 
 console.log(counted);
+
+console.log('');
+console.log('==============');
+console.log('');
+
+// Sort массива
+// Создайте функцию sortDesc, которая будет сортировать переданный массив чисел по убыванию
+// Основные требования:
+// 1. Функция должна называться sortDesc
+// 2. Функция должна отсортировать исходный массив в порядке убывания
+// 3. Исходный массив не должен измениться
+
+const sortDescArr = [7, 20, 34, 56, 72, 3, 11];
+
+function compare(a,b) {
+  if (a > b) return -1;
+  if (a == b) return 0;
+  if (a < b) return 1;
+}
+
+function sortDesc(arr) {
+  const newArr = arr.map(el => el);
+  // sort меняет первоначальный массив, правильно ли я промапил сначала?
+  return newArr.sort(compare);
+}
+
+console.log(sortDesc(sortDescArr));
+console.log(sortDescArr, 'исходный массив');
+
+
+console.log('');
+console.log('==============');
+console.log('');
+
+
+// Плоский массив
+// Создай функцию flatArray, которая будет принимать массив чисел, элементами которого могут быть 
+// другие одномерные массивы и преобразует его в плоский массив 
+// Основные требования:
+// 1. Исходный массив может содержать массивы в качестве элементов
+// 2. Функция должна создать плоский массив из исходного массива
+// 3. Функция должна отсортировать результирующий массив по возрастанию
+// 4. Исходный массив не должен измениться
+
+const flatArrayArr = [46, 75, [5, 34, 80], 6, -1]
+
+function flatArray(arr) {
+  const newArr = arr.map(el => el);
+
+  const newArr1 = newArr.forEach((el, i) => {
+    if (Array.isArray(el)) {
+      const arrDel = newArr.splice(i, 1);
+      arrDel.forEach(el => newArr.push(el));
+    }
+  });
+
+  return newArr;
+}
+
+console.log(flatArray(flatArrayArr));
+console.log(flatArrayArr, 'исходный массив');
